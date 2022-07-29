f70c9e302606ee09507c9c9264d6c04de37bbee2
airwithi <110253813+airwithi@users.noreply.github.com>
2022-07-29 17:47:09 -0500

    Update docb.py

--- docb.py
+++ docb.py
@@ -6,6 +6,10 @@ for v in range(0:25):
 print("hola como andas")
 
 z = 235
-for d in range(0:z)
+for d in range(0:z):
     print("Como andas")
 
+for az in range(0:15):
+    print("otro bucle del 4to commit")
+    
+    
